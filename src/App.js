dd2c7d8169f93829bf843a8be863098be3a1453d
PrakashJogi <toprakashjogi@gmail.com>
2022-08-19 20:41:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,54 +10,10 @@ import Footer from "./W_Main/footer.js";
 import Home from "./Pages/Home.js";
 import About from "./Pages/About.js";
 import Contact from "./Pages/Contact.js";
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
-
-// let Dummy_array = [
-//     {
-//         id: "e1",
-//         title: "EMI Loan",
-//         amount: 5000,
-//         date: new Date(2022,8,15)
-//     },
-//     {
-//         id: "e2",
-//         title: "Mobile Reacharge",
-//         amount: 300,
-//         date: new Date(2022,8,16)
-//     }  
-// ];
+import {BrowserRouter,Routes,Route,} from "react-router-dom";
 
 const App = () => 
 {
-    // const [All_expense,setAllExpense] = useState(Dummy_array);
-
-    // useEffect( () => {              //Here start using Api for DB
-    //     fetch("").then(
-    //         response =>
-    //         {
-    //             return response.json()
-    //         }
-    //     ).then(
-    //         data =>
-    //         {
-    //             console.log(data);
-    //             setAllExpense(data);
-    //         }
-    //     )
-    // },[]);              //here array bracket for not looping APi data
-
-    // const AddExpenseData = (expense_data) =>
-    // {
-    //     const updateExpense = [expense_data, ...All_expense];
-    //     setAllExpense(updateExpense);
-    // }
-
-
-
     return(
         <div className="divv">
             <Header />              
